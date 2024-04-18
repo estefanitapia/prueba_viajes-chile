5127959253e8d44a26a626bd9b8f49f4136341bd
estefanitapia <estefani.tapia.godoy@gmail.com>
2024-04-17 20:21:16 -0400

    agregando jquery y javascript

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,19 @@
 // Tooltip en redes sociales
 
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
+$(document).ready(function () {});
 
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
+
+// Enviar mensaje en contacto
+  $(document).ready(function () {
+    $('[data-bs-toggle="tooltip"]').tooltip();
+  
+  $("#enviaCorreo").click(function () {
+    alert("El correo fue enviado exitosamente...");
+  });
+});
